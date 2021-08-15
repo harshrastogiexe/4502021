282982df92c07febfbc718bf5b43144c43d4db8b
Harsh Rastogi <rastogiharsh04@gmail.com>
2021-08-15 16:49:03 +0530

    Merge Shorted Array Without Extra Space

--- src/sdeSheet/Day1/mergeSortedArray.js
+++ src/sdeSheet/Day1/mergeSortedArray.js
@@ -4,26 +4,36 @@
  * @param {number[]} array2 Array
  */
 function mergeArray(array1, array2) {
-  let gap = Math.ceil((array1.length + array2.length) / 2);
+  let lenA = array1.length,
+    lenB = array2.length;
+
+  const calcgap = (initgap) => (initgap <= 1 ? 0 : Math.ceil(initgap / 2));
+  let gap = calcgap(lenA + lenB);
+
   while (gap > 0) {
-    let i = 0;
-    for (; i + gap < array1.length; i++)
-      if (array1[i + gap] < array1[i])
+    let i;
+    for (i = 0; gap + i < lenA; i++)
+      if (array1[gap + i] < array1[i])
         [array1[i], array1[i + gap]] = [array1[i + gap], array1[i]];
 
-    let j = gap > array1.length ? gap - array1.length : 0;
-    for (; i < array1.length && j < array2.length; i++, j++)
+    let j = gap > lenA ? gap - lenA : 0;
+    for (; i < lenA && j < lenB; i++, j++)
       if (array2[j] < array1[i])
         [array2[j], array1[i]] = [array1[i], array2[j]];
 
-    if (j < array2.length) {
-      for (let j = 0; j + gap < array2.length; j++) {
-        [array2[j], array2[j + gap]] = [array2[j + gap], array2[j]];
-      }
-    }
-    gap = gap >> 1;
+    if (j < lenB)
+      for (; j + gap < lenB; j++)
+        if (array2[j + gap] < array2[j])
+          [array2[j], array2[j + gap]] = [array2[j + gap], array2[j]];
+
+    gap = calcgap(gap);
   }
-  console.log(...array1, ...array2);
+
+  return [array1, array2];
 }
 
-mergeArray([3, 27, 38, 43], [9, 10, 82]);
+const result = mergeArray(
+  [3, 27, 38, 43, 100, 201],
+  [9, 10, 82, 100, 203, 404]
+);
+mergeArray([1, 5, 9, 10, 15, 20], [2, 3, 8, 13]);
