c52a6b957f8792a5eea6cae5c6f6489b70b5f294
Harsh Rastogi <rastogiharsh04@gmail.com>
2021-08-16 10:16:50 +0530

    Merge Interval

--- src/sdeSheet/Day1/mergeIntervals.js
+++ src/sdeSheet/Day1/mergeIntervals.js
@@ -2,36 +2,24 @@
  *
  * @param {number[][]} intervals Interval
  */
-function mergeInterval(intervals) {
-  intervals.sort(([timeA], [timeB]) => timeA - timeB);
+function mergeInterval(intervals, sorted = false) {
+  !sorted && intervals.sort(([timeA], [timeB]) => timeA - timeB);
+  // console.log(intervals);
+  const stack = [];
+  stack.push(intervals[0]);
 
-  const len = intervals.length;
-  const merged = [[intervals[0][0], intervals[0][1]]];
-
-  const isMerging = (intvA, intvB) => {
-    return intvA[1] >= intvB[0];
-  };
-
-  const merge = (intvA, intvB) => {
-    return [intvA[0], Math.max(intvA[1], intvB[1])];
-  };
-
-  for (let i = 1; i < len; i++) {
-    if (
-      isMerging(merged[merged.length - 1], intervals[i]) &&
-      merged[merged.length - 1][1] < intervals[i][1]
-    )
-      merged[merged.length - 1][1] = intervals[i][1];
-    else merged.push(intervals[i]);
-    // if (intervals[i][0] <= merge[merge.length - 1][1]) {
-    //   merge[merge.length - 1][1] = intervals[i][1];
-    // } else {
-    //   merge.push(intervals[i]);
-    // }
+  for (let i = 1; i < intervals.length; i++) {
+    if (stack[stack.length - 1][1] < intervals[i][0]) {
+      stack.push(intervals[i]);
+    } else {
+      stack[stack.length - 1][1] = Math.max(
+        stack[stack.length - 1][1],
+        intervals[i][1]
+      );
+    }
   }
 
-  console.log(merged);
-  return merged;
+  console.log(stack);
 }
 
 mergeInterval([
